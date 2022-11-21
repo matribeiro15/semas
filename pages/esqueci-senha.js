635f0c498996fbfcc087a3f076b5cfb3c759acbc
leticia <vfleticia544@gmail.com>
2022-11-20 23:25:38 -0400

    ...

--- pages/esqueci-senha.js
+++ pages/esqueci-senha.js
@@ -1,16 +1,17 @@
 import {FormDefault,InputText,ButtonDefault,} from  "../components/du-objects.js"
-import HeadLeh from  'next/head'
+import HeadLeh from  '../components/head.js'
 import LogoPrefeitura from '../components/logo-prefeitura.js'
 import Link from "next/link"
 import  MdOutlineFamilyRestroom  from "react-icons/md";
 export default function Esquecisen(){
   return (
+    <>
     <HeadLeh>
       <title>Esqueci senha | SEMAS</title>
     </HeadLeh>
     <div className="w-full flex flex-row min-h-screen">
       <div className="flex-auto flex flex-col items-center justify-center p-3">
-        <h1 className="text-cor_principal-600 text-4xl mb-5 font-bold ">Esqueci minha senha</h1>
+        <h1 className="text-cor_principal-600 text-4xl mb-5 font-bold ">Esqueci Minha Senha</h1>
         <FormDefault API="users/create" id="formCad" resetAfterSend={true} className="max-w-[400px] w-full flex flex-wrap gap-x-14 justify-center items-stretch">
           <InputText fatherClassName="w-full flex flex-col items-center" name="digite" label="Digite CPF" className="max-w-[500px] w-full text-last rounded-xl" />
           <div className="w-full text-center mt-8 shine">
@@ -21,5 +22,6 @@ export default function Esquecisen(){
 
       </div>
     </div>
+    </>
   )
 }
